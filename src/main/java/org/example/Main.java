356444d55734cb4b61ed5581509a4ea8b3fbb863
replakcan <alper.makcan@gmail.com>
2024-11-19 14:29:47 +0300

    answers forChallengers questions on Main w/comment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,11 @@ public class Main {
     public static void main(String[] args) {
 
         Bedroom bedroom = new Bedroom("istanblue", new Wall("doğu"), new Wall("kuzey"), new Wall("batı"), new Wall("güney"), new Ceiling(4, PaintColor.RED), new Bed("geleneksel", 3, 4, 2, 1), new Lamp(LampType.NORMAL, true, 5), new Wardrobe(2, 3, 250), new Carpet(16, 9, PaintColor.GREEN));
+
+        /*
+        ForChallengers.1- Bedroom is a Room (inheritance),
+        ForChallengers.2- Ceiling, and three walls,
+        ForChallengers.3-
+         */
     }
 }
